208ae81cb5cf76a9488387ef14e94c9e7b95834e
ap3120 <ygbe@hotmail.fr>
2022-08-29 10:57:14 +0100

    version 1

--- app.js
+++ app.js
@@ -32,3 +32,27 @@ window.onscroll = () => {
     }
     prevScrollPos = currentScrollPos;
 }
+
+//Show course content
+const itContent = document.querySelector('.it-career-switch');
+const codecademyContent = document.querySelector('.codecademy');
+const nearContent = document.querySelector('.near');
+const itBtn = document.querySelector('.it-career-switch-btn');
+const codecademyBtn = document.querySelector('.codecademy-btn');
+const nearBtn = document.querySelector('.near-btn');
+
+const showHideCourse = (hideBtn1, hideBtn2, showBtn, hide1, hide2, show) => {
+    hideBtn1.classList.remove('btn-active');
+    hideBtn2.classList.remove('btn-active');
+    showBtn.classList.add('btn-active');
+    hide1.classList.remove('course-description-active');
+    hide1.classList.add('course-description-inactive');
+    hide2.classList.remove('course-description-active');
+    hide1.classList.add('course-description-inactive');
+    show.classList.remove('course-description-inactive');
+    show.classList.add('course-description-active');
+}
+
+itBtn.onclick = function() {showHideCourse(codecademyBtn, nearBtn, itBtn, codecademyContent, nearContent, itContent)};
+codecademyBtn.onclick = function() {showHideCourse(itBtn, nearBtn, codecademyBtn, itContent, nearContent, codecademyContent)};
+nearBtn.onclick = function() {showHideCourse(itBtn, codecademyBtn, nearBtn, itContent, codecademyContent, nearContent)};
