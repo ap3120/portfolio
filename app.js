c0b0dccc5a0beb6c54be2be9a952f28cb2ea28ce
dev <ygbe@hotmail.fr>
2022-10-22 18:04:35 +0100

    commit

--- app.js
+++ app.js
@@ -32,27 +32,94 @@ window.onscroll = () => {
     }
     prevScrollPos = currentScrollPos;
 }
-
-//Show course content
-const itContent = document.querySelector('.it-career-switch');
-const codecademyContent = document.querySelector('.codecademy');
-const nearContent = document.querySelector('.near');
-const itBtn = document.querySelector('.it-career-switch-btn');
-const codecademyBtn = document.querySelector('.codecademy-btn');
-const nearBtn = document.querySelector('.near-btn');
-
-const showHideCourse = (hideBtn1, hideBtn2, showBtn, hide1, hide2, show) => {
-    hideBtn1.classList.remove('btn-active');
-    hideBtn2.classList.remove('btn-active');
-    showBtn.classList.add('btn-active');
-    hide1.classList.remove('course-description-active');
-    hide1.classList.add('course-description-inactive');
-    hide2.classList.remove('course-description-active');
-    hide1.classList.add('course-description-inactive');
-    show.classList.remove('course-description-inactive');
-    show.classList.add('course-description-active');
+// home fadeout
+//
+const coursesSection = document.getElementById('courses');
+const homeSection = document.getElementById('home');
+const initialCoursesSectionPosition = coursesSection.offsetTop;
+window.onscroll = () => {
+    let courseSectionPosition = coursesSection.offsetTop - window.pageYOffset;
+    homeSection.style.opacity = 2*courseSectionPosition/initialCoursesSectionPosition - 1;
 }
 
-itBtn.onclick = function() {showHideCourse(codecademyBtn, nearBtn, itBtn, codecademyContent, nearContent, itContent)};
-codecademyBtn.onclick = function() {showHideCourse(itBtn, nearBtn, codecademyBtn, itContent, nearContent, codecademyContent)};
-nearBtn.onclick = function() {showHideCourse(itBtn, codecademyBtn, nearBtn, itContent, codecademyContent, nearContent)};
+// projects section
+
+const projects = [
+    {
+        title:'Web 3.0 Application',
+        text:'Designed and deployed a Web 3.0 website able to pair with Metamask, to send Ethereum on the Goerli network and to interact with smart contracts. The smart contracts were tested locally using Hardhat and deployed on Alchemy.',
+        image:'./images/ethapp.png',
+        githubLink:'https://github.com/ap3120/ethApp',
+        url:'http://ethapp.herokuapp.com/',
+    },
+
+    {
+        title:'Django Database website',
+        text:'Database website using the Django framework where users can register and login to create lists and add items to them. Used PostgresQL database in the backend and tested the application with coverage. Performed continuous integration using Github workflow',
+        image:'./images/django.jpeg',
+        githubLink: '',
+        url: ''
+    },
+    {
+        title: 'Sudoku game',
+        text:'Created a sudoku game using plain CSS and JavaScript.',
+        image:'./images/sudoku.png',
+        githubLink:'https://github.com/ap3120/sudoku/',
+        url:'http://gecko-sudoku.herokuapp.com/',
+    },
+    {
+        title:'Pancakeswap clone',
+        text:'I created this static webpage that replicates the frontend of the Pancakeswap official website when I was learning CSS.',
+        image:'./images/pancakeswap.png',
+        githubLink:'https://github.com/ap3120/pancakeswap_clone',
+        url:'https://ap3120.github.io/pancakeswap_clone/',
+    },
+];
+
+for (let project of projects){
+    const card = document.createElement('div');
+    const overlay = document.createElement('div');
+    const title = document.createElement('h3');
+    const text = document.createElement('p');
+    const image = document.createElement('img');
+    card.classList.add('project-card');
+    overlay.classList.add('overlay');
+    title.innerHTML = project.title;
+    text.innerHTML = project.text;
+    if (project.image.length > 0) {
+        const image = document.createElement('img');
+        image.classList.add('image');
+        image.classList.add('image-blur');
+        image.src = project.image;
+        card.appendChild(image);
+    }
+    overlay.appendChild(title);
+    overlay.appendChild(text);
+    if (project.githubLink.length > 0 || project.url.length > 0) {
+        const linksContainer = document.createElement('div');
+        if (project.githubLink.length > 0){
+            const githubLinkTag = document.createElement('a');
+            const githubIcon = document.createElement('i');
+            githubLinkTag.href = project.githubLink;
+            githubLinkTag.target = '_blank';
+            githubIcon.classList.add('fa-brands');
+            githubIcon.classList.add('fa-github');
+            githubLinkTag.appendChild(githubIcon);
+            linksContainer.appendChild(githubLinkTag);
+        }
+        if (project.url.length > 0) {
+            const urlLinkTag = document.createElement('a');
+            const urlIcon = document.createElement('i');
+            urlLinkTag.href = project.url;
+            urlLinkTag.target = '_blank';
+            urlIcon.classList.add('fa-solid');
+            urlIcon.classList.add('fa-up-right-from-square');
+            urlLinkTag.appendChild(urlIcon);
+            linksContainer.appendChild(urlLinkTag);
+        }
+        overlay.appendChild(linksContainer);
+    }
+        
+    card.appendChild(overlay);
+    document.getElementsByClassName('projects-container')[0].appendChild(card);
+}
